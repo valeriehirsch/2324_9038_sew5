46502af21b410e699ec1c0b25536df36a09359c7
Valerie Hirsch <9038@htl.rennweg.at>
2024-01-22 09:09:55 +0100

    init lab

---
+++ sew5_sem1p/ue03/labyrinth.py
@@ -0,0 +1,24 @@
+import argparse
+import time
+
+
+class LabyrinthSolver:
+    """
+    >>> solver = LabyrinthSolver("l2.txt", 5, 5, False, 0, 0)
+    >>> solver.solve_labyrinth()
+    nbr of solutions: 486
+    """
+
+    border_char = '#'
+    visited_char = 'X'
+    target_char = 'A'
+
+    def __init__(self, filename, x_start, y_start, should_print, print_time, delay):
+        self.labyrinth = self.load_labyrinth(filename)
+        self.x_start = x_start
+        self.y_start = y_start
+        self.should_print = should_print
+        self.print_time = print_time
+        self.delay = delay
+        self.solutions = set()
+
