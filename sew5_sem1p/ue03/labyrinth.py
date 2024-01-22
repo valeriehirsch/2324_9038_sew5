71e46a23aff389437bea0a60d2818f89058dfce7
Valerie Hirsch <9038@htl.rennweg.at>
2024-01-22 09:10:50 +0100

    lab auslesen

--- sew5_sem1p/ue03/labyrinth.py
+++ sew5_sem1p/ue03/labyrinth.py
@@ -22,3 +22,10 @@ class LabyrinthSolver:
         self.delay = delay
         self.solutions = set()
 
+    def load_labyrinth(self, filename):
+        """ reads the labyrinth from the file"""
+        with open(filename, 'r') as file:
+            labyrinth = [line.strip() for line in file]
+        return labyrinth
+
+   
